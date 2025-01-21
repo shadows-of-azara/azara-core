f9185029a806a7179c7c1c3d5d6396636b97373a
dorfink <dorfink@github.com>
2025-01-20 22:24:18 -0500

    Added "Arcane Bolt" Ability and scroll w/o 25% mana restore on crit.

---
+++ datascripts/abilities/basic-attack/arcane-bolt.ts
@@ -0,0 +1,33 @@
+import { std } from "wow/wotlk";
+import { createAbility } from "../../functions/createAbility";
+
+// Create Spell //
+const SPELL = std.Spells.create("azara-core", "arcane-bolt-ability")
+    .Name.enGB.set("Arcane Bolt")
+    .Description.enGB.set("Sends a shadowy bolt at the enemy, causing $s1 Shadow damage.")
+    .Icon.setPath("Spell_Arcane_Starfire")
+    .PreventionType.SILENCE.set()
+    .FacingCasterFlags.set("SPELL_FACING_FLAG_INFRONT")
+    .Attributes.set(['NOT_SHAPESHIFTED'])
+    .InterruptFlags.set(['ON_MOVEMENT','ON_PUSHBACK','ON_INTERRUPT_CAST','ON_INTERRUPT'])
+    .SchoolMask.set(['PHYSICAL','SHADOW'])
+    .Visual.set(271)
+    .DefenseType.set(1)
+    .Speed.set(20)
+    .CastTime.setSimple(1700)
+    .Cooldown.GlobalTime.set(1500)
+    .Cooldown.GlobalCategory.set(133)
+    .Power.Type.MANA.set()
+    .Power.CostPercent.set(10)
+    .Range.setSimple(0, 30)
+    .Effects.addMod(x => x
+        .Type.SCHOOL_DAMAGE.set()
+        .DamageBase.set(12)
+        .DamageDieSides.set(5)
+        .DamagePerLevel.set(0.4000000059604645)
+        .BonusMultiplier.set(0.14000000059604645)
+        .ImplicitTargetA.UNIT_TARGET_ENEMY.set()
+    )
+
+// Export spell as an ability //
+export const ARCANE_BOLT = createAbility(SPELL.ID)
