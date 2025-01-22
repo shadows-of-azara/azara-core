397e7365aa101824b42b42dccdf6763bbecfc32d
dorfink <dorfink@github.com>
2025-01-21 19:16:56 -0500

    Added 25% mana restore on crit to "Arcane Bolt" ability and removed gm-items folder

--- datascripts/abilities/basic-attack/arcane-bolt.ts
+++ datascripts/abilities/basic-attack/arcane-bolt.ts
@@ -1,21 +1,19 @@
 import { std } from "wow/wotlk";
 import { createAbility } from "../../functions/createAbility";
 
-// Create 25% Mana Restore //
-const MANA_RESTORE = std.Spells.create("azara-core", "mana-regen")
+// Create 25% Mana Restore Spell //
+const MANA_RESTORE = std.Spells.create("azara-core", "mana-restore")
     .Name.enGB.set("Mana Restore")
-    .Range.set(1)
-    .Visual.set(7265)
-    .Visual.modRefCopy(visual => {
-        visual.PrecastKit.set(3790)
-    })
-    .Effects.addMod(eff => eff
-        .Type.ENERGIZE_PCT.set()
+    .Description.enGB.set("Restores $s1 mana.")
+    .Attributes.set(["IS_ABILITY","IS_PASSIVE","IS_HIDDEN_IN_SPELLBOOK"])
+    .SchoolMask.set(["PHYSICAL"])
+    .Power.Type.MANA.set()
+    .Effects.addMod(x=>x
+        .Type.ENERGIZE.set()
         .PowerType.MANA.set()
-        .PowerPctBase.set(2.5)
+        .ChainAmplitude.set(1)
         .ImplicitTargetA.UNIT_CASTER.set()
     )
-       // .Attributes.IS_HIDDEN_FROM_LOG.set(true)
 
 // Create Spell //
 const SPELL = std.Spells.create("azara-core", "arcane-bolt-ability")
@@ -24,10 +22,10 @@ SPELL.Name.enGB.set("Arcane Bolt")
     .Description.enGB.set("Sends a magical bolt at the enemy, causing $s1 Arcane damage.")
     .Icon.setPath("Spell_Arcane_Starfire")
     .PreventionType.SILENCE.set()
-    .FacingCasterFlags.set("SPELL_FACING_FLAG_INFRONT")
-    .Attributes.set(['NOT_SHAPESHIFTED'])
-    .InterruptFlags.set(['ON_MOVEMENT','ON_PUSHBACK','ON_INTERRUPT_CAST','ON_INTERRUPT'])
-    .SchoolMask.set(['PHYSICAL','SHADOW'])
+    .FacingCasterFlags.set(["SPELL_FACING_FLAG_INFRONT"])
+    .Attributes.set(["NOT_SHAPESHIFTED"])
+    .InterruptFlags.set(["ON_MOVEMENT","ON_PUSHBACK","ON_INTERRUPT_CAST","ON_INTERRUPT"])
+    .SchoolMask.set(["PHYSICAL","ARCANE"])
     .Visual.set(271)
     .DefenseType.set(1)
     .Speed.set(20)
@@ -45,14 +43,17 @@ SPELL.Name.enGB.set("Arcane Bolt")
         .BonusMultiplier.set(0.14000000059604645)
         .ImplicitTargetA.UNIT_TARGET_ENEMY.set()
     )
-    .Effects.addMod(x => x
-        .Type.APPLY_AURA.set()
-        .Aura.PROC_TRIGGER_SPELL.set()
-        .TriggeredSpell.set(MANA_RESTORE.ID)
-        .ImplicitTargetA.UNIT_CASTER.set()
-    )
-    .Proc.HitMask.CRITICAL.set(true)
-    .Proc.Chance.set(100)
-    .Proc.TypeMask.DAMAGE.set(true)
-    .Proc.PhaseMask.HIT.set(true)
-    .Proc.TriggerMask.DONE_SPELL_MAGIC_DAMAGE_CLASS_POSITIVE.set(true)
+    // Restores 25% of Ability cost on crit. //
+    .InlineScripts.OnDamageLate((spell,damage,info,type,crit) => {
+        const caster = ToUnit(spell.GetCaster());
+        const cost = spell.GetPowerCost();
+        if (! spell || ! info || ! caster || ! cost)
+            return;
+
+        if (! crit)
+            return;
+
+        const value = Math.round(cost * 0.25);
+
+        caster.CastCustomSpell(caster,GetID("Spell","azara-core","mana-restore"),true,value)
+    })
