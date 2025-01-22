28ff44b019ee80826df5a1d591ec37cf0b137c91
Magic <61628974+magic62@users.noreply.github.com>
2025-01-22 16:50:52 -0500

    "Rend" now costs 10% of the players base mana instead of 10 rage. Moved "Ability Vendor" to Northshire. (#8)
    Co-authored-by: dorfink <dorfink@github.com>

--- datascripts/creatures/ability-vendor.ts
+++ datascripts/creatures/ability-vendor.ts
@@ -32,4 +32,4 @@ export const ABILITY_VENDOR = std.CreatureTemplates.create("azara-core", "abilit
     .Rank.ELITE.set()
     .Models.addIds(MODEL.ID)
     .NPCFlags.VENDOR.set(true)
-    .Spawns.add("azara-core", "ability-vendor", { map: 0, x: -8630.144531, y: -552.395020, z: 145.078461, o: 1.791834 })
+    .Spawns.add("azara-core", "ability-vendor", { map: 0, x: -8940.504883, y: -141.321716, z: 83.484573, o: 0.844251},)
